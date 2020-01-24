6b32d8000b1f182842dc6ec05b02822570c6b083
Cameron Bolyard <bolyardcameron@gmail.com>
2020-01-24 14:17:03 -0500

    Template Complete

--- kcompapp/app/components/App.vue
+++ kcompapp/app/components/App.vue
@@ -1,67 +1,30 @@
-<template>
-    <Page>
-        <ActionBar>
-            <GridLayout width="100%" columns="auto, *">
-                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
-                <Label class="title" text="Welcome to NativeScript-Vue!"  col="1"/>
-            </GridLayout>
-        </ActionBar>
-
-        <RadSideDrawer ref="drawer">
-            <StackLayout ~drawerContent backgroundColor="#ffffff">
-                <Label class="drawer-header" text="Drawer"/>
-
-                <Label class="drawer-item" text="Item 1"/>
-                <Label class="drawer-item" text="Item 2"/>
-                <Label class="drawer-item" text="Item 3"/>
-            </StackLayout>
-
-            <GridLayout ~mainContent columns="*" rows="*">
-                <Label class="message" :text="msg" col="0" row="0"/>
-            </GridLayout>
-        </RadSideDrawer>
-    </Page>
+<template lang="html">
+  <RadSideDrawer ref="drawer" drawerLocation="Left" gesturesEnabled="true" :drawerTransition="transition">
+    <StackLayout ~drawerContent backgroundColor="#ffffff">
+      <slot name="drawerContent"></slot>
+    </StackLayout>
+    <Frame ~mainContent ref="drawerMainContent">
+      <slot name="mainContent"></slot>
+    </Frame>
+  </RadSideDrawer>
 </template>
 
-<script >
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
-    }
+<script>
+    import { SlideInOnTopTransition } from 'nativescript-ui-sidedrawer';
+    
+    export default {
+        data () {
+          return {
+            transition: new SlideInOnTopTransition()
+          }
+        }
   }
 </script>
 
-<style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+<style scoped lang="scss">
+    // Start custom common variables
+    @import '~@nativescript/theme/scss/variables/blue';
+    // End custom common variables
 
-    .title {
-        text-align: left;
-        padding-left: 16;
-    }
-
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
-
-    .drawer-header {
-        padding: 50 16 16 16;
-        margin-bottom: 16;
-        background-color: #53ba82;
-        color: #ffffff;
-        font-size: 24;
-    }
-
-    .drawer-item {
-        padding: 8 16;
-        color: #333333;
-        font-size: 16;
-    }
+    // Custom styles
 </style>
